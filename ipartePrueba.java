278712eb45eaa6fbd887e43b49cfeb8af1bbd4b3
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-04-26 18:39:58 +0200

    Iteracion y deteccion de numeros pares/impares

---
+++ ipartePrueba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author drohne
+ */
+public class ipartePrueba{
+     public static void main(String []args){
+        for (int x=1;x<=10;x++) {
+          if(x%2==0){
+    			System.out.println("El numero "+ x +" es par");
+            }else{
+                System.out.println("El numero "+ x +" es impar");
+                }
+        }
+    }
+}
