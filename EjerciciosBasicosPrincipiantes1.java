456fc66204d34b64d8b4e46caa46b1ab858159da
rabejon <rabejon@kernet.es>
2018-07-10 09:33:17 +0200

    Ejercicios Basicos Principiantes 1

---
+++ EjerciciosBasicosPrincipiantes1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosbasicosprincipiantes1;
+
+/**
+ *
+ * @author drohne
+ */
+public class EjerciciosBasicosPrincipiantes1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n = 5;
+        double a = 6.5;
+        char c = 'p';
+        int cConversion = (int) c;
+        System.out.println(c);
+        System.out.println(n+a);
+        System.out.println(n-a);
+        System.out.println( cConversion);
+    }
+    
+}
